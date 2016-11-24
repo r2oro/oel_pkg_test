6c06a013c4abc7650e580a9a556b9144fbb61186
212481589 <artur.pioro@ge.com>
2016-11-24 11:24:46 +0100

    initial

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+pkg_src_location = 'https://s3.amazonaws.com/solution-automation-folder/qualys'
+pkg = 'qualys-cloud-agent.x86_64.rpm'
+local_image = "#{Chef::Config['file_cache_path']}/#{pkg}"
+
+remote_file 'qualys-cloud-agent-image' do
+  path local_image
+  source "#{pkg_src_location}/#{pkg}"
+end
+
+package 'qualys-cloud-agent' do
+  source local_image
+end
